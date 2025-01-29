ee68837a8fa760b952d8f84dc3888b77f8d103a3
PAPPU RAM <118615240+PRChoudhary@users.noreply.github.com>
2025-01-29 13:28:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,80 @@
-# IP-Address-Finder
-With the help of splay tree data structure, we would create a tree whose nodes are embedded with the Ip address of the device that are connect to a specific network router. In our code we have taken 11 devices connected to one network router and so there would be some common part in the Ip address of each of the devices. Now, router gets some specific data packets from the net which is supposed to be given to a specified device and so it uses searching operation to find the correct Ip address. To increase the speed of this process we use splay tress for searching and inserting the Ip addresses. It is the fastest data structure for searching operation. Therefore, the router sends the data packet to the specified Ip address when multiple devices are connected. Here we have used the random function to input the data packets so that there is no input function required and the processes is completely automatic as it takes place in network router.
+# Blog Mingle
 
+A sleek and user-friendly blogging platform that provides engaging content for users to explore, interact with, and enjoy.
+
+---
+
+## Table of Contents
+
+1. [Features](#features)
+2. [Usage](#usage)
+3. [User Functionality](#user-functionality)
+4. [Admin Functionality](#admin-functionality)
+
+---
+
+## Features
+
+### User Features:
+
+- **Authentication**: Users can sign in or sign up with Google authentication or standard email/password login.
+- **View Posts**: Browse articles, tutorials, and insights on various tech topics.
+- **Search & Filter Posts**: Search for specific posts and filter them based on categories, tags, or other criteria.
+- **Comment on Posts**: Users can comment on posts (supports CRUD operations: Create, Read, Update, Delete).
+- **User Profile**: Users can update their profile (CRUD functionality), including email, username, and profile picture.
+- **Recent Articles**: View the most recent articles on the homepage.
+- **Light/Dark Mode**: Users can toggle between light and dark themes for better readability and personalization.
+
+### Admin Features:
+
+- **Admin Dashboard**: Admins can view a dashboard to manage the application, which includes user statistics, recent posts, and more.
+- **User Management**: Admins can view, add, update, and delete user profiles.
+- **Post Management**: Admins can create, update, and delete posts.
+- **Comment Management**: Admins can moderate and manage comments on posts.
+- **Analytics**: View key metrics such as the number of registered users, number of posts, likes, and comments.
+
+---
+
+## Usage
+
+1. **For Users**:
+
+   - Sign up/sign in using your Google account or traditional email/password.
+   - Browse and filter posts based on various topics.
+   - Interact with posts by leaving comments and liking comments.
+   - Toggle between light and dark modes for the best reading experience.
+
+2. **For Admin**:
+   - Sign in as an admin to access the admin dashboard.
+   - View and manage user accounts, posts, and comments.
+   - Review key statistics on user registrations, post activity, and more.
+
+---
+
+## User Functionality
+
+- **Sign In/Sign Up**: Users can register with Google or traditional login.
+- **Post Interaction**: View, like, and comment on posts.
+- **Profile Management**: Edit user profile details (email, username, profile picture).
+- **Search and Filter**: Find posts through search and apply filters (by categories, tags).
+- **Light/Dark Mode**: Toggle between light and dark themes for a customized user experience.
+
+---
+
+## Admin Functionality
+
+- **Admin Dashboard**: Access a dashboard with essential metrics.
+- **User Management**: Admins can view and manage user profiles (create, update, delete).
+- **Post Management**: Admins can manage posts (create, update, delete).
+- **Comment Moderation**: Admins can moderate user comments (approve, delete, or update).
+- **Statistics**: View analytics related to user activity, post interaction, and more.
+
+---
+
+## Live Link
+
+Check out the live version of **Blog Mingle** here: [https://blog-mingle.onrender.com](https://blog-mingle.onrender.com)
+
+---
+
+Feel free to contribute or provide feedback. Enjoy using **Blog Mingle**!
---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,201 @@
+import { Button, Select, TextInput } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import PostCard from '../components/PostCard';
+
+export default function Search() {
+    const [sidebarData, setSidebarData] = useState({
+        searchTerm: '',
+        sort: 'desc',
+        category: 'uncategorized',
+    });
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [showMore, setShowMore] = useState(false);
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        const sortFromUrl = urlParams.get('sort');
+        const categoryFromUrl = urlParams.get('category');
+        if (searchTermFromUrl || sortFromUrl || categoryFromUrl) {
+            setSidebarData({
+                ...sidebarData,
+                searchTerm: searchTermFromUrl,
+                sort: sortFromUrl,
+                category: categoryFromUrl,
+            });
+        }
+
+        const fetchPosts = async () => {
+            setLoading(true);
+            const searchQuery = urlParams.toString();
+            const res = await fetch(`/api/post/getposts?${searchQuery}`);
+            if (!res.ok) {
+                setLoading(false);
+                return;
+            }
+            const data = await res.json();
+            setPosts(data.posts);
+            setLoading(false);
+            if (data.posts.length === 9) {
+                setShowMore(true);
+            } else {
+                setShowMore(false);
+            }
+        };
+        fetchPosts();
+    }, [location.search]);
+
+    const handleChange = (e) => {
+        if (e.target.id === 'searchTerm') {
+            setSidebarData({ ...sidebarData, searchTerm: e.target.value });
+        }
+        if (e.target.id === 'sort') {
+            const order = e.target.value || 'desc';
+            setSidebarData({ ...sidebarData, sort: order });
+        }
+        if (e.target.id === 'category') {
+            const category = e.target.value || 'uncategorized';
+            setSidebarData({ ...sidebarData, category });
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', sidebarData.searchTerm);
+        urlParams.set('sort', sidebarData.sort);
+        urlParams.set('category', sidebarData.category);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    const handleShowMore = async () => {
+        const numberOfPosts = posts.length;
+        const startIndex = numberOfPosts;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/post/getposts?${searchQuery}`);
+        if (!res.ok) {
+            return;
+        }
+        const data = await res.json();
+        setPosts([...posts, ...data.posts]);
+        if (data.posts.length === 9) {
+            setShowMore(true);
+        } else {
+            setShowMore(false);
+        }
+    };
+
+    // Reset filters logic
+    const handleResetFilters = () => {
+        setSidebarData({
+            searchTerm: '',
+            sort: 'desc',
+            category: 'uncategorized',
+        });
+        // Clear the URL search parameters and navigate to show all posts
+        const resetParams = new URLSearchParams();
+        navigate(`/search?${resetParams.toString()}`);
+    };
+
+    return (
+        <div className='flex flex-col md:flex-row'>
+            <div className="p-7 border-b md:border-r md:min-h-screen border-gray-500 w-full md:w-1/3">
+                <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+                    {/* Search Term Section */}
+                    <div className="flex flex-col md:flex-row items-center gap-2 w-full">
+                        <label className="font-semibold w-full md:w-1/3">Search Term:</label>
+                        <TextInput
+                            placeholder="Search..."
+                            id="searchTerm"
+                            type="text"
+                            value={sidebarData.searchTerm}
+                            onChange={handleChange}
+                            className="w-full md:w-2/3"
+                        />
+                    </div>
+
+                    {/* Sort Section */}
+                    <div className="flex flex-col md:flex-row items-center gap-2 w-full">
+                        <label className="font-semibold w-full md:w-1/3">Sort:</label>
+                        <Select
+                            onChange={handleChange}
+                            value={sidebarData.sort}
+                            id="sort"
+                            className="w-full md:w-2/3"
+                        >
+                            <option value="desc">Latest</option>
+                            <option value="asc">Oldest</option>
+                        </Select>
+                    </div>
+
+                    {/* Category Section */}
+                    <div className="flex flex-col md:flex-row items-center gap-2 w-full">
+                        <label className="font-semibold w-full md:w-1/3">Category:</label>
+                        <Select
+                            onChange={handleChange}
+                            value={sidebarData.category}
+                            id="category"
+                            className="w-full md:w-2/3"
+                        >
+                            <option value="uncategorized">Uncategorized</option>
+                            <option value="reactjs">React.js</option>
+                            <option value="nextjs">Next.js</option>
+                            <option value="javascript">JavaScript</option>
+                        </Select>
+                    </div>
+
+                    {/* Apply Filters Button */}
+                    <div className="flex justify-center w-full mt-4">
+                        <Button type="submit" outline gradientDuoTone="tealToLime" className="w-full md:w-2/3">
+                            Apply Filters
+                        </Button>
+                    </div>
+                </form>
+
+                {/* Reset Filters Button */}
+                <div className="flex justify-center w-full mt-4">
+                    <Button
+                        onClick={handleResetFilters}
+                        outline
+                        color="gray"
+                        className="w-full md:w-2/3 flex items-center justify-center"
+                    >
+                        Reset Filters
+                    </Button>
+                </div>
+            </div>
+
+            <div className='w-full'>
+                <h1 className='text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5 '>
+                    Posts results:
+                </h1>
+                <div className='p-7 flex flex-wrap gap-4'>
+                    {!loading && posts.length === 0 && (
+                        <p className='text-xl text-gray-500'>No posts found.</p>
+                    )}
+                    {loading && <p className='text-xl text-gray-500'>Loading...</p>}
+                    {!loading &&
+                        posts &&
+                        posts?.map((post) => <PostCard key={post._id} post={post} />)}
+                    {showMore && (
+                        <button
+                            onClick={handleShowMore}
+                            className='text-teal-500 text-lg hover:underline p-7 w-full'
+                        >
+                            Show More
+                        </button>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
